aa7f24b40f761860c664dba8c20c235fa8256c1a
Bryan Boukari <bryan.boukari@gmail.com>
2019-04-29 00:48:28 +0200

    WIP save article

--- app.rb
+++ app.rb
@@ -7,18 +7,19 @@ get "/" do
   @comments = COMMENTS
   erb :index
 end
-get "/comment/:index" do
+get "/article/:index" do
   @element = DB[params['index'].to_i]
   @comments = COMMENTS[params['index'].to_i]
   erb :comment
 end
 get "/write-article" do
-  @database = DB
-  @comments = COMMENTS
   erb :article
 end
 post "/article" do
   @article = params[:article]
+  #@data = DB
+  #@data << @article
+  #DB = @data
   #Ajouter a DB
   erb :wip
 end
